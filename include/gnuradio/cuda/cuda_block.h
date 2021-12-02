2663641fb3d733bdefef206ada0bdbfbed7c628d
Josh Morman <jmorman@gnuradio.org>
2021-12-02 17:10:06 -0500

    Convenience header for cuda_block
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

---
+++ include/gnuradio/cuda/cuda_block.h
@@ -0,0 +1,30 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2021 Josh Morman
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#ifndef _INCLUDED_CUDA_BLOCK_H
+#define _INCLUDED_CUDA_BLOCK_H
+
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
+namespace gr {
+    
+class cuda_block
+{
+protected:
+    cudaStream_t d_stream;
+    int d_min_grid_size;
+    int d_block_size;
+public: 
+    cuda_block() {};
+};
+
+} // namespace gr
+#endif
