a11d852054bf0d102b051d9704f2090a0f8c6669
Josh Morman <jmorman@gnuradio.org>
2021-12-03 15:52:21 -0500

    Add stream creation into cuda_block.h
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- include/gnuradio/cuda/cuda_block.h
+++ include/gnuradio/cuda/cuda_block.h
@@ -11,19 +11,21 @@
 #ifndef _INCLUDED_CUDA_BLOCK_H
 #define _INCLUDED_CUDA_BLOCK_H
 
+#include <gnuradio/cuda/cuda_error.h>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 
 namespace gr {
-    
+
 class cuda_block
 {
 protected:
     cudaStream_t d_stream;
     int d_min_grid_size;
     int d_block_size;
-public: 
-    cuda_block() {};
+
+public:
+    cuda_block() { check_cuda_errors(cudaStreamCreate(&d_stream)); };
 };
 
 } // namespace gr
